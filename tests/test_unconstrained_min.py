36358b246939c78184c535e2ca622010a00e6ad5
Tomer Shefler <sheflertomer@gmail.com>
2023-06-01 20:25:21 +0300

    tests/exmaples.py -> tests/examples.py

--- tests/test_unconstrained_min.py
+++ tests/test_unconstrained_min.py
@@ -1,6 +1,6 @@
 import unittest
 import numpy as np
-from tests import exmaples
+from tests import examples
 from src import unconstrained_min
 
 
@@ -13,19 +13,19 @@ class TestUnconstrainedMin(unittest.TestCase):
             self.u.line_search_min(minimizer ,f, x0, max_iter=max_iter)
 
     # def test_f1(self):
-    #     self._test_f(exmaples.f1)
+    #     self._test_f(examples.f1)
 
     # def test_f2(self):
-    #     self._test_f(exmaples.f2)
+    #     self._test_f(examples.f2)
 
     def test_f3(self):
-        self._test_f(exmaples.f3)
+        self._test_f(examples.f3)
 
     def test_rosenbrock(self):
-        self._test_f(exmaples.rosenbrock, x0=np.array([-1, 2], dtype='int64'), max_iter=10000)
+        self._test_f(examples.rosenbrock, x0=np.array([-1, 2], dtype='int64'), max_iter=10000)
 
     def test_e(self):
-        self._test_f(exmaples.e_func)
+        self._test_f(examples.e_func)
 
     def test_vect(self):
-        self._test_f(exmaples.vect)
+        self._test_f(examples.vect)
